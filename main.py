ca7bea0aafffb7d900393335b247cec5c4115f5d
Andrew Guernsey <344agg21@students.olatheschools.com>
2021-03-24 16:11:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+
+
+#
+#👋 Hi, I’m @jayhawker6
+#👀 I’m interested in Python, Dart, Java, And Kotlin.
+#🌱 I’m currently learning Kotlin.
+#💞️ I’m looking to collaborate on My small game i've been working on, ByteBase.
+#📫 You can reach me by messaging or creating bug/issue reports or pull requests.
+#-And remember kids:
+#
+#"People who care about code effeciency are weebs.
+#My code is ugly and yours can be too!"
+#
+#      -Ghandi
+#
+
+for x in range(0,1000):
+	time.sleep(.01)
+	print('I am %d going on %d!' % (x, x+1))
+print("I am 1000 and I'm tired of counting!")
